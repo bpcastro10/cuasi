d89aae27a4f8789d1c224662592caf028b738ef7
bpcastro1 <bpcastro1@espe.edu.ec>
2025-05-16 07:14:51 -0500

    com

--- proy/microclientes/microclientes/src/main/java/com/proyecto/microclientes/repository/PersonaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.proyecto.microclientes.repository;
-
-import com.proyecto.microclientes.entity.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, String> {} 
--- proy/microcuentas/microcuentas/src/main/java/com/proyecto/microcuentas/repository/CuentaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.proyecto.microcuentas.repository;
-
-import com.proyecto.microcuentas.entity.Cuenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CuentaRepository extends JpaRepository<Cuenta, String> {} 
